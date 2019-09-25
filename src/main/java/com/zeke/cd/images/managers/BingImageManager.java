e822e73952e18fa1585805528033450826082b10
ZekeWong <760553478@qq.com>
2019-09-25 11:37:39 +0800

    title(V1.1)：增加Bing图片每日抓心获取功能的测试代码。

--- src/main/java/com/zeke/cd/images/managers/BingImageManager.java
+++ src/main/java/com/zeke/cd/images/managers/BingImageManager.java
@@ -1,6 +1,7 @@
 package com.zeke.cd.images.managers;
 
 import com.google.gson.Gson;
+import com.intellij.concurrency.JobScheduler;
 import com.intellij.openapi.diagnostic.Logger;
 import com.zeke.cd.images.BingImageBean;
 import com.zeke.cd.images.HttpUtils;
@@ -13,12 +14,17 @@ import java.io.File;
 import java.io.FileFilter;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 /**
  * Bing图片的管理类
+ *
  * @author king.Z
  * @version 1.0
  * @since 2019-05-03 14:34
@@ -32,19 +38,21 @@ public class BingImageManager extends BaseImageManager {
     private static final String BING_REQEST = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=%s&n=1";
     private File cacheBingPath;
     private File cacheImageFilePath;
+    private ImageUpdaterTask updaterTask;
     private BingImageManager() {
         super();
         initCachePath();
+        updaterTask = new ImageUpdaterTask();
     }
 
     private void initCachePath() {
         File pluginFilePath = getPluginFilePath();
         String path = pluginFilePath.getAbsolutePath();
         String cacheFilePath;
-        if(PluginDefaultConfig.SANDBOX_MODE){
-           cacheFilePath = Utils.join(File.separator, new String[]{path, "neck_protect_cache","bing"});
-        }else{
-           cacheFilePath = Utils.join(File.separator, new String[]{pluginFilePath.getParent(), "neck_protect_cache","bing"});
+        if (PluginDefaultConfig.SANDBOX_MODE) {
+            cacheFilePath = Utils.join(File.separator, new String[]{path, "neck_protect_cache", "bing"});
+        } else {
+            cacheFilePath = Utils.join(File.separator, new String[]{pluginFilePath.getParent(), "neck_protect_cache", "bing"});
         }
         cacheBingPath = new File(cacheFilePath);
         if (!cacheBingPath.exists()) {
@@ -58,7 +66,7 @@ public class BingImageManager extends BaseImageManager {
 
     @Override
     public URL getImageUrl() {
-        if(mImageUrl != null){
+        if (mImageUrl != null) {
             Pattern winPattern = getWinPattern();
             Pattern unixPattern = getUnixPattern();
             if (winPattern.matcher(mImageUrl.toString()).matches() ||
@@ -84,14 +92,14 @@ public class BingImageManager extends BaseImageManager {
      * 初始化Bing图片模块
      */
     public void init() {
-        preFeatchImage();
+        requestBingImage();
     }
 
     /**
      * 预拉取一次当日bing图片
      */
-    private void preFeatchImage() {
-        //TODO V1.1 增加请求页数处理
+    private void requestBingImage() {
+        LOG.info("requestBingImage");
         String url = String.format(BING_REQEST, "0");
         HttpUtils.sendHttpRequest(url, HttpUtils.GET, new HttpUtils.CallBack() {
             @Override
@@ -124,9 +132,9 @@ public class BingImageManager extends BaseImageManager {
 
         String imageFullUrl = BING_HOST.concat(url);
         String bingPath = cacheBingPath.getAbsolutePath();
-        String imageFilePath = Utils.join(File.separator, new String[]{bingPath,cacheFileName});
+        String imageFilePath = Utils.join(File.separator, new String[]{bingPath, cacheFileName});
         cacheImageFilePath = new File(imageFilePath);
-        if(!cacheImageFilePath.exists()){
+        if (!cacheImageFilePath.exists()) {
             cleanCacheDir(cacheBingPath);
             HttpUtils.downLoadFile(imageFullUrl, cacheImageFilePath);
             try {
@@ -136,12 +144,12 @@ public class BingImageManager extends BaseImageManager {
                 mImageUrl = DefaultImageManager.getDefaultUrl();
             }
             updateConfigDataImageUrl(mImageUrl);
-        }else{
+        } else {
             LOG.info("图片已存在:" + cacheFileName);
         }
     }
 
-    private void updateConfigDataImageUrl(URL url){
+    private void updateConfigDataImageUrl(URL url) {
         ConfigState configState = IConfigService.getInstance().getState();
         configState.setRemindImageUrl(url.toString());
         IConfigService.getInstance().updateState(configState);
@@ -152,7 +160,7 @@ public class BingImageManager extends BaseImageManager {
         String suffix = ".png";
         Pattern pattern = getSuffixPattern();
         Matcher matcher = pattern.matcher(url);
-        if(matcher.matches()){
+        if (matcher.matches()) {
             suffix = matcher.group(1);
         }
         return md5.concat(suffix);
@@ -160,15 +168,16 @@ public class BingImageManager extends BaseImageManager {
 
     /**
      * 清理缓存图片目录下之前所有的图片文件
+     *
      * @param fileDir Bing图片缓存目录
      */
     @SuppressWarnings("ResultOfMethodCallIgnored")
-    private void cleanCacheDir(File fileDir){
+    private void cleanCacheDir(File fileDir) {
         fileDir.listFiles(new CacheImageCleanerFilter());
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
-    class CacheImageCleanerFilter implements FileFilter{
+    class CacheImageCleanerFilter implements FileFilter {
         @Override
         public boolean accept(File file) {
             file.delete();
@@ -178,13 +187,64 @@ public class BingImageManager extends BaseImageManager {
 
     private static Pattern getWinPattern() {
 		return Pattern.compile("^.+neck_protect_cache\\/bing\\/.+$");
-	}
+    }
 
     private static Pattern getUnixPattern() {
-		return Pattern.compile("^.+neck_protect_cache\\\\bing\\\\.+$");
-	}
+        return Pattern.compile("^.+neck_protect_cache\\\\bing\\\\.+$");
+    }
 
     private static Pattern getSuffixPattern() {
-		return Pattern.compile("^.*(\\.jpg|\\.jpeg|\\.png).+$");
-	}
+        return Pattern.compile("^.*(\\.jpg|\\.jpeg|\\.png).+$");
+    }
+
+    private class ImageUpdaterTask {
+        long checkPeriod = 6;
+        //记录当前日期时间，用于不重启IDE的情况下更新每日图片
+        private String currentDate = "";
+        private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        private final ThreadLocal<ScheduledFuture> SCHEDULED_FUTURE_CONTEXT = new ThreadLocal<>();
+
+        public ImageUpdaterTask() {
+            tickCurrentDate();
+        }
+
+        public void init() {
+            restart();
+        }
+
+        public void restart() {
+            destroy();
+            ScheduledFuture scheduledFuture = JobScheduler.getScheduler()
+                    .scheduleWithFixedDelay(new CheckRunnable(),checkPeriod, checkPeriod, TimeUnit.HOURS);
+            SCHEDULED_FUTURE_CONTEXT.set(scheduledFuture);
+        }
+
+        private void destroy() {
+            ScheduledFuture existScheduledFuture = SCHEDULED_FUTURE_CONTEXT.get();
+            if (existScheduledFuture != null) {
+                existScheduledFuture.cancel(true);
+                SCHEDULED_FUTURE_CONTEXT.remove();
+            }
+        }
+
+        String tickCurrentDate(){
+            this.currentDate = dateFormat.format(new Date());
+            LOG.info("当前系统时间："+ this.currentDate);
+            return this.currentDate;
+        }
+
+        class CheckRunnable implements Runnable {
+            @Override
+            public void run() {
+                String date = dateFormat.format(new Date());
+                LOG.info("Bing Checker run： current=" + date + "; lastTime=" + currentDate);
+                if(currentDate.equals(date)){
+                    //日期未变  不需要重新获取bing图片
+                }else{
+                    requestBingImage();
+                }
+            }
+        }
+    }
 }
