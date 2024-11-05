19def8bab30af045b0ce179ef2c82e1aa97359d3
kivensolo-homePC <kiven6969@gmail.com>
2024-11-05 22:49:56 +0800

    Title(refactor):Change PluginIcons to kt. Cause：Deprecated method IconLoader.getIcon(String) is invoked in PluginIcons.Actions.<clinit>() Solution：Use getIcon(String,ClassLoader). Effect: N/A Reviewer: N/A 附加说明：N/A

--- src/main/java/icons/PluginIcons.kt
+++ src/main/java/icons/PluginIcons.kt
@@ -1,20 +1,21 @@
-package icons;
+package icons
 
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
+import com.intellij.openapi.util.IconLoader.getIcon
+import com.intellij.util.ReflectionUtil
 
 /**
  * 插件自定义图标
  *
  * @author King.Z
  * @version 1.0
- * @see <a href="https://www.jetbrains.org/intellij/sdk/docs/reference_guide/work_with_icons_and_images.html">IconsAndImages</a>
+ * @see [IconsAndImages](https://www.jetbrains.org/intellij/sdk/docs/reference_guide/work_with_icons_and_images.html)
+ *
  * @since 2019-05-01
+ * @update 2024-11-05
  */
-public class PluginIcons{
-
-    public static final class Actions {
-        public static final Icon ZEKE_HEX = IconLoader.getIcon("/icons/color_table.png");
+class PluginIcons {
+    object Actions {
+        private const val hexPath = "/icons/color_table.png"
+        val ZEKE_HEX = getIcon(hexPath, ReflectionUtil.getGrandCallerClass() ?: error(hexPath))
     }
 }
