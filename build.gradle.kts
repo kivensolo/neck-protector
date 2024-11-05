09c95f098c741310cdd68a8fb932e970f4898f20
kivensolo-homePC <kiven6969@gmail.com>
2024-11-05 21:43:58 +0800

    Adjust IDEA compatibility range to 212-231*

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,14 @@ plugins {
 //    id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.intellij")
 }
+
+ext.set("plugin_version","1.1.212-231")
+
 repositories {
     maven("https://maven.aliyun.com/repository/central")
     maven("https://maven.aliyun.com/repository/public")
     maven("https://maven.aliyun.com/repository/google")
-    mavenCentral()
+//    mavenCentral()
 }
 
 intellij {
@@ -40,15 +43,20 @@ tasks{
         pluginId.set("com.zeke.wong.neck-protect")
         sinceBuild.set("212")
         untilBuild.set("231.*")
-        version.set("1.1.212-231")
+        version.set(ext.get("plugin_version") as String)
         changeNotes.set("""
           <ul>
             <li>v1.0 Init.</li>
             <li>v1.1 Automatically update Bing image every day.</li>
             <li>v1.1.1 Fix bugs in version 1.1.</li>
-            <li>v1.1.212 Adjust IDEA compatibility range to 212-231*</li>
+            <li>v1.1.202-211 Compiled with Java11 and Adjust IDEA compatibility range to 202-211.* .</li>
+            <li>v1.1.212-231 Compiled with Java17 and Adjust IDEA compatibility range to 212-231.*</li>
           </ul>
           """)
+    }
+
+    jar {
+        archiveFileName.set("neck_protect_${ext.get("plugin_version") as String}.jar")
     }
 
     signPlugin {
