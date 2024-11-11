530bab32baa2f93fc0bf4710d96c829f81033eb9
kivensolo-homePC <kiven6969@gmail.com>
2024-11-11 22:21:02 +0800

    Title(fix):修复color_table.png不能被加载的问题； Cause：kotlin代码的变量访问问题导致。 Solution：先还原，后续再更改。 Effect: N/A Reviewer: N/A 附加说明：N/A

---
+++ src/main/java/icons/PluginIcons.java
@@ -0,0 +1,22 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * 插件自定义图标
+ *
+ * @author King.Z
+ * @version 1.0
+ *
+ * @see <a href="https://www.jetbrains.org/intellij/sdk/docs/reference_guide/work_with_icons_and_images.html">Working with Icons</a>
+ *
+ * @since 2019-05-01
+ * @update 2024-11-05
+ */
+public class PluginIcons{
+    public static final class Actions {
+        public static final Icon ZEKE_HEX = IconLoader.getIcon("/icons/color_table.png", PluginIcons.class.getClassLoader());
+    }
+}
