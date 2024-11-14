adaa06e811d2c5412f71a1b97161638a5c2783bb
zhi.wang <zhi.wang@starcor.com>
2024-11-14 13:59:30 +0800

    Title(optimization):Refactor the method to get the DataContext asynchronously. Cause：Deprecated method DataManager.getDataContextFromFocus() is invoked Solution：Use the onSuccess and onError methods instead of doWhenDone and doWhenRejected。 Effect: N/A Reviewer: N/A 附加说明：N/A

--- src/test/java/com/zeke/cd/images/OpenImageConsumerTest.java
+++ src/test/java/com/zeke/cd/images/OpenImageConsumerTest.java
@@ -1,16 +1,14 @@
 package com.zeke.cd.images;
 
 import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
-import com.intellij.util.Consumer;
 
 @SuppressWarnings("ALL")
 public class OpenImageConsumerTest extends LightPlatformCodeInsightFixtureTestCase {
 
     public void testAccept() {
-        DataManager.getInstance().getDataContextFromFocus()
-                .doWhenDone((Consumer<DataContext>) (dataContext -> new OpenImageConsumer().accept(dataContext)))
-                .doWhenRejected((Consumer<String>) System.out::println);
+        DataManager.getInstance().getDataContextFromFocusAsync()
+                .onSuccess(dataContext -> new OpenImageConsumer().accept(dataContext))
+                .onError(LOG::error);
     }
 }
